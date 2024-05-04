e9d14fdfe04e0117dd2580225f63228d60894aac
ihsanerturkk <ihsanerturk2026@gmail.com>
2024-05-04 23:06:38 +0300

    add araba class

---
+++ lib/first/araba.dart
@@ -0,0 +1,15 @@
+class Araba {
+  String? id;
+  String? name;
+
+  @override
+  String toString() {
+    return 'Araba{id: $id, name: $name}';
+  }
+
+}
+
+
+
+
+
---
+++ lib/first/firts_dart.dart
@@ -0,0 +1,15 @@
+
+
+
+import 'araba.dart';
+
+void main(){
+
+  var araba1 = Araba();
+  araba1.id = "birinci-araba";
+  araba1.name = "TOGG";
+
+  print("Araba Adı: ${araba1.name}");
+  print(araba1);
+
+}
